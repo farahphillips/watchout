bd3af35226b1f7a271feb4ee3432355dfcf8c7fa
fmaredia <farhana.maredia@gmail.com>
2015-06-19 19:23:59 -0500

    Fourth Commit. Player is draggable.

--- watchout.js
+++ watchout.js
@@ -1,54 +1,50 @@
-// Draw the enemies in an svg element.
-
 var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var svg = d3.selectAll("body")
+var body = d3.selectAll("body")
           .append("svg")
+          .attr("class", "box")
           .attr("width", 1000)
-          .attr("height", 1000);
+          .attr("height", 500);
 
-var cartman = svg.selectAll(".cartman")
+var cartman = body.selectAll(".cartman")
           .data(numArr)
           .enter()
           .append("svg:image")
           .attr("xlink:href", "cartman.png")
           .attr("width", 100)
           .attr("height", 100)
-          .attr("x", function() { return 600 * Math.random() } )
-          .attr("y", function() { return 600 * Math.random() } )
+          .attr("x", function() { return 200 * Math.random() } )
+          .attr("y", function() { return 200 * Math.random() } )
           .style('position', 'absolute');
 
-// Make a differently-colored dot to represent the player.
-var butters = svg.selectAll(".butters")
+var butters = body.selectAll(".butters")
           .data([1])
           .enter()
           .append("svg:image")
           .attr("xlink:href", "butters.png")
+          .attr("class", "butters")
           .attr("width", 150)
           .attr("height", 100)
           .attr("x", function(d) { return d * 400 })
           .attr("y", function(d) { return d * 400 });
 
-// Make the player draggable.
-
-// drag = d3.behavior.drag()
-//        .on("drag", mover);
-
-
-
-// Make it so that the enemies move to a new random location every second.
+body.on("mousemove", function() {
+  var mouse = d3.mouse(this);
+  body.select(".butters")
+          .style("x", mouse[0])
+          .style("y", mouse[1]);
+  });
 
 var loop = function(cartman) {
   cartman.transition().duration(750)
-  .attr("x", function() { return 800 * Math.random() } )
-  .attr("y", function() { return 800 * Math.random() } )
+  .style("x", function() { return 1000 * Math.random() } )
+  .style("y", function() { return 450 * Math.random() } )
   .each("end", function() { loop(d3.select(this)); });
 }
 
 loop(cartman);
 
 // Detect when a enemy touches you.
-
 
 
 
